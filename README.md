ba87bdd19e7d1aa1921282f1be644843c4271a1e
Omega <omeganju@gmail.com>
2016-12-29 14:50:12 +0800

    appearance of vector again

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Data Structure and Algorithms Codes
+
+数据结构与算法（C++版）示例代码，Xcode 8.1 编译运行
+
+包含 Target
+
+- [x] 斐波那契数 fabonacci 算法的三种实现
+- [ ] 向量模版 vector 的实现
+- [ ] 列表
--- vector/vector_bubblesort_fast.h
+++ vector/vector_bubblesort_fast.h
@@ -9,5 +9,5 @@
 #pragma once
 
 template <typename T> //向量的起泡排序
-void Vector<T>::bubbleSort ( Rank lo, Rank hi ) //assert: 0 <= lo < hi <= size
-{ while ( lo < ( hi = bubble ( lo, hi ) ) ); } //逐趟做扫描交换，直至全序
+void Vector<T>::bubbleSort(Rank lo, Rank hi) //assert: 0 <= lo < hi <= size
+{ while (lo < (hi = bubble(lo, hi))); } //逐趟做扫描交换，直至全序
