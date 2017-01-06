fbd2f805805ab08d849cd75ac82786eb5c5e251a
Omega <omeganju@gmail.com>
2017-01-06 15:05:58 +0800

    stack implementation finished

---
+++ stack/stack.h
@@ -0,0 +1,13 @@
+//
+//  stack.h
+//  Data Structure Algorithm
+//
+//  Created by BB8 on 17/1/6.
+//  Copyright © 2017年 BB8. All rights reserved.
+//
+
+#pragma once
+
+
+#include "stack@vector.h" //由向量派生的栈
+//#include "stack@list.h" //由列表派生的栈
---
+++ stack/stack_test.h
@@ -0,0 +1,16 @@
+//
+//  stack_test.h
+//  Data Structure Algorithm
+//
+//  Created by BB8 on 17/1/6.
+//  Copyright © 2017年 BB8. All rights reserved.
+//
+
+#pragma once
+
+#include <iostream>
+
+#include "../_share/util.h"
+#include "../uniprint/print.h"
+
+#include "stack.h"
